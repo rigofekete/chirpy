b7357817a56d92dabd4e1eec8730b0778c812d6f
rigofekete <fabricecosteira@gmail.com>
2025-10-28 00:21:35 +0100

    set fileserver to use /app as root. Added readiness endpoint at /healthz with new handler

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 
 func main() {
+	const filepathRoot = "./app"
 	const port = "8080"
 
 	mux := http.NewServeMux()
@@ -16,8 +17,19 @@ func main() {
 		Addr: 		":" + port,
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
-	log.Printf("Serving on port: %s\n", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(http.StatusText(http.StatusOk))
+	if err != nil {
+		log.Printf("error writing body: %w", err)
+	}
 }
