d11b11600c8ae3c5519910a2f121111a868ff2c2
rigofekete <fabricecosteira@gmail.com>
2025-11-19 00:00:21 +0100

    updated GET chirps endpoint to accept query parameter sort for asc and desc sorting. Added logic for sorting in get all chirps handler

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -2,21 +2,46 @@ package main
 
 import (
 	"net/http"
+	"sort"
+
 	"github.com/google/uuid"
 )
 
 
 func (cfg *apiConfig) handlerChirpsGetAll(w http.ResponseWriter, r *http.Request) {
-	dbChirps, err := cfg.db.GetChirps(r.Context())
+	dbChirps, err := cfg.db.GetChirps(r.Context()) 
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't get chirps from DB", err)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get Author ID chirps from DB", err)
 		return
 	}
 
-	chirpsJSON := []Chirp{}
+	query := "asc"
+	queryParam := r.URL.Query().Get("sort")
+	if queryParam == "desc" {
+		query = "desc"
+	}
+
+
+
+	authorID := uuid.Nil
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString != "" {
+		authorID, err = uuid.Parse(authorIDString)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid author ID", err)
+			return
+		}
+	} 
+
+
+	chirps := []Chirp{}
 
 	for _, dbChirp := range dbChirps {
-		chirpsJSON = append(chirpsJSON, Chirp{
+		if authorID != uuid.Nil && authorID != dbChirp.UserID {
+			continue
+		}
+
+		chirps = append(chirps, Chirp{
 			ID: 		dbChirp.ID,
 			CreatedAt: 	dbChirp.CreatedAt,
 			UpdatedAt: 	dbChirp.UpdatedAt,
@@ -25,21 +50,29 @@ func (cfg *apiConfig) handlerChirpsGetAll(w http.ResponseWriter, r *http.Request
 		})
 	}
 
+	sort.Slice(chirps, func(i, j int) bool {
+		if query == "desc" {
+			return chirps[i].CreatedAt.After(chirps[j].CreatedAt)
+		} else {
+			return chirps[i].CreatedAt.Before(chirps[j].CreatedAt)
+		}
+	})
 
-	respondWithJSON(w, http.StatusOK, chirpsJSON)
+
+	respondWithJSON(w, http.StatusOK, chirps)
 }
 
 
 func (cfg *apiConfig) handlerChirpsGetSingle(w http.ResponseWriter, r *http.Request) {
-	chirpID := r.PathValue("chirpID")
+	chirpIDString := r.PathValue("chirpID")
 
-	chirpUUID, err := uuid.Parse(chirpID) 
+	chirpID, err := uuid.Parse(chirpIDString) 
 	if err != nil {
 		respondWithError(w, http.StatusBadRequest, "Invalid chirp ID", err)
 		return
 	}
 
-	dbChirp, err := cfg.db.GetChirp(r.Context(), chirpUUID) 
+	dbChirp, err := cfg.db.GetChirp(r.Context(), chirpID) 
 	if err != nil {
 		respondWithError(w, http.StatusNotFound, "Couldn't get chirp from DB", err)
 		return
